db8b4d92e7aa097d5c54b641391724085576291c
Leticia Figueiredo <leticia.ofigueiredo@gmail.com>
2025-02-24 07:25:38 -0300

    Apontando pro banco

--- managers/tasks_manager.py
+++ managers/tasks_manager.py
@@ -1,22 +1,38 @@
-import logging, json
-from managers.config import config as c
-from managers.s3_manager import get_from_s3
-
-logging.basicConfig(level=logging.INFO, format='%(message)s')
+from managers.log_manager import log, log_erro
+from managers.model_manager import RPATable
+from managers.integration_manager import open_postgres_conn
 
 
 def fetch_pending_tasks():
-    ''' Converte as informações da variavel de ambiente EXCEL_JSON para uma lista de dicionários '''
-
-    try:
-        local_file_path = get_from_s3()
-        if not local_file_path:
+    ''' Busca tarefas pendentes no banco (status = 0) '''
+    
+    with open_postgres_conn('Buscando tasks no banco') as session:
+        try:
+            log('----> Procurando no banco tarefas para processar com status 0')
+            tasks = (
+                session.query(RPATable)
+                .filter(RPATable.status == 0)                
+                .all()
+            )
+            
+            task_dicts = [task_to_dict(task) for task in tasks]
+            return task_dicts
+        
+        except Exception as e:
+            log_erro(f'Falha em trazer tarefas com status 0. Erro: {e}')
             return []
+        
 
-        with open(local_file_path, 'r') as json_file:
-            return json.load(json_file)
+def task_to_dict(task):
+    ''' Converte a instancia do RPATable em dicionario.'''
 
-    except Exception as e:
-        logging.info(f'Falha na leitura do JSON pelo EXCEL_JSON: {e}')
-        return []   
-
+    return {
+        'id': task.id,
+        'nome_do_processo': task.nome_do_processo,
+        'status': task.status,
+        'task': task.task,
+        'data_inicio': task.data_inicio,
+        'data_fim': task.data_fim,
+        'observacoes': task.observacoes,
+        'id_execucao': task.id_execucao
+    }        
