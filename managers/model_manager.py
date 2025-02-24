cbc7f5529b261aa3822e55c9b6119618cb06ab93
Leticia Figueiredo <leticia.ofigueiredo@gmail.com>
2025-02-24 07:23:32 -0300

    Ajustando ORM

--- managers/model_manager.py
+++ managers/model_manager.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, Integer, Text, String, TIMESTAMP
 from managers.config import config as c
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.dialects.postgresql import TIMESTAMP as PG_TIMESTAMP
 
 Base = declarative_base()
 
@@ -13,9 +14,10 @@ class RPATable(Base):
     nome_do_processo = Column(Text)
     status = Column(Integer) 
     task = Column(JSONB)
-    data_inicio = Column(TIMESTAMP)
-    data_fim = Column(TIMESTAMP)
-    observacoes = Column(String(200)) 
+    data_inicio = Column(PG_TIMESTAMP(timezone=False))
+    data_fim = Column(PG_TIMESTAMP(timezone=False))
+    observacoes = Column(String(200))
+    id_execucao = Column(String(10000)) 
 
 
 class DashTable(Base):
