ca49ebc9bc6cef72f4c0d9246575183c71308d70
Leticia Figueiredo <leticia.ofigueiredo@gmail.com>
2025-02-24 07:25:27 -0300

    Ajustando card

--- managers/teams_manager.py
+++ managers/teams_manager.py
@@ -1,18 +1,24 @@
 import requests
 from managers.config import config as c
+from managers.log_manager import  log, log_erro
 
 
-def send_card(content):
+def send_card(len_tasks, len_errors):
     ''' Monta e envia um card para o teams | Estrutura do infos '''
    
+    log(f'----> Enviando card no Teams')
+   
     payload = {
-        'area': c.t_area,
-        'nomeRPA': c.t_rpa,
-        'retornoRPA': content
+        'canal': c.t_area,
+        'quantidadeEmpresa': len_tasks,
+        'quantidadeErro': len_errors
     }   
 
     try:
-        requests.post(c.t_url, json=payload)
+        req = requests.post(c.t_url, json=payload)
+        log(f'--> Status Code: {req.status_code}')
+        log(f'--> Content: {req.content}')
+        
     
-    except:
-        print('[E] Falha ao enviar card no Teams')
+    except Exception as e:
+        log_erro(f'Falha ao enviar card no Teams. Erro: {e}')
